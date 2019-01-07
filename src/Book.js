338bc546e4bae70d5b1b50971ce2b1c707fa940c
Richard Craven <Richard.rcraven85@gmail.com>
2019-01-06 21:52:28 -0600

    components generated

---
+++ src/Book.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import { ReactComponent } from '*.svg';
+
+function Book (props) {
+    console.log('in book instance, props are ', props)
+    return (
+        <div>book</div>
+    )
+}
+
+export default Book;
---
+++ src/Shelf.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+// import { ReactComponent } from '*.svg';
+
+class Shelf extends Component{
+    render(){
+        return (
+            <div>shelf</div>
+        )
+    }
+}
+
+export default Shelf;
